f18be1eb158b15c3031516d1e2373cdd6cab97b4
Hauke Bartsch <HaukeBartsch@gmail.com>
2021-05-20 11:13:49 +0200

    provide a very basic stub for bash

---
+++ components/Workflow-Image-AI/src/templates/stub.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+input="${1}/input"
+output="${1}/output"
+
+###############################################
+# Put your code here
+# Data: ${input}
+# Structured Information: ${input}/descr.json
+###############################################
+
